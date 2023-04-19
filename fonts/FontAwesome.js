c1f74248c9d4867a41426055908fb7777fff9586
akekaphol101 <akekaphol101@gmail.com>
2023-04-19 16:38:46 +0700

    Build New Font

---
+++ fonts/FontAwesome.js
@@ -0,0 +1,8 @@
+module.exports = {
+	FontAwesome: {
+		normal: __dirname + "/ThaiFonts/FontAwesome_regular.ttf",
+		bold: __dirname + "/ThaiFonts/FontAwesome_regular.ttf",
+		italics: __dirname + "/ThaiFonts/FontAwesome_regular.ttf",
+		bolditalics: __dirname + "/ThaiFonts/FontAwesome_regular.ttf",
+	},
+};
