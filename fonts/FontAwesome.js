0776ef876182cfefcaf31a5b3e2d8ee9a61e5123
akekaphol101 <akekaphol101@gmail.com>
2023-04-25 16:40:57 +0700

    DEploy

--- fonts/FontAwesome.js
+++ fonts/FontAwesome.js
@@ -1,7 +1,7 @@
 module.exports = {
 	FontAwesome: {
-		normal: __dirname + "/ThaiFonts/FontAwesome_regular.ttf",
-		bold: __dirname + "/ThaiFonts/FontAwesome_regular.ttf",
+		normal: __dirname + "/ThaiFonts/fa-regular-400.ttf",
+		bold: __dirname + "/ThaiFonts/fa-solid-900.ttf",
 		italics: __dirname + "/ThaiFonts/FontAwesome_regular.ttf",
 		bolditalics: __dirname + "/ThaiFonts/FontAwesome_regular.ttf",
 	},
